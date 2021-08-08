694031be76eafeffe18c538bd92f2866cfa1bdf2
unknown <cristiano.enke@gmail.com>
2021-08-08 14:50:23 -0300

    Refactoring code

--- src/App.js
+++ src/App.js
@@ -1,74 +1,14 @@
-//Maneira usando class
-/*
-import React, { Component } from "react"
+import React from "react"
+import Toggle from './Toggle'
+import { ToggleProvider} from "./ContextProvider"
 
 const App = () => {
   return (
-    <div>
-      <Toggle />
-    </div>
-  )
-}
-
-
-class Toggle extends Component{
-  state = {
-      toggle: false
-  };
-
-  handleToggle = () => {
-      this.setState(prevState => {
-          return{
-              toggle: !prevState.toggle
-          }
-      });
-  };
-
-    render() {
-    return (
-        <div>
-         <p>The Status is:{this.state.toggle ? "ON" : "OFF"}</p>
-         <button onClick={this.handleToggle}> Toggle </button>
-    </div>
-    )
-    
-     }
-}
-
-export default App;
-*/
-
-// Maneira sem classe
-import React, { useState } from "react"
-
-const App = () => {
-  return (
-    <div>
+    <ToggleProvider>
       <Toggle />
       <p> Other Status: </p>
-    </div>
+    </ToggleProvider>
   )
 }
 
-const Toggle = () => {
-    const [toggle, setToggle] = useState(false);
-
-    const handleToggle = () => setToggle(prevState=>!prevState)
-
-    return (
-        <div>
-            <ToggleStatus toggle={toggle} />
-            <ToggleButton handleToggle={handleToggle} />
-        </div>
-    )
-}
-
-const ToggleStatus = ({toggle}) => (
-    <p>The Status is:{toggle ? "ON" : "OFF"}</p>
- );
-
-const ToggleButton = ({handleToggle}) => (
-    <button onClick={handleToggle}> Toggle </button>
-
-    )
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-import React, { Fragment } from "react"
-import ReactDOM from "react-dom"
-import "./styles.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const element = 'Digital Innovation'
-const element2 = <h1>Olá Turma =D</h1>
-
-function App() {
-  return (
-    <div>
-      {element}
-      {element2}
-    </div>
-  )
-}
-
-const rootElement = document.getElementById("root")
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<App />, document.getElementById("root"));
